d00fc059e216774baacde0152b4de3f0b9eddd75
siri-004 <chandanasiri004@gmail.com>
2025-12-19 08:10:44 +0530

    Fix bubble sort logic to improve efficiency
    Fixed the bubble sort implementation by moving the flag check outside of the inner loop.

--- Sorting_Techniques/1_bubble_sort.c
+++ Sorting_Techniques/1_bubble_sort.c
@@ -2,11 +2,13 @@
 void BubbleSort(int a[],int n){
     int temp,flag;
     for(int i=0;i<n;i++){
+        flag=0;
         for(int j=0;j<n-1-i;j++){
             if(a[j]>a[j+1]){
                 temp=a[j];
                 a[j]=a[j+1];
                 a[j+1]=temp;
+                flag=1;
             }
             if(flag==0){
                 break;
